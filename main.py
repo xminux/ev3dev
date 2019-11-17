9734e3b1f2c88e00eaa74d00d66d376028be6772
Minuk Chung <audrey@Minukui-MacBook-Pro.local>
2019-11-17 20:03:34 +0900

    New file

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+from ev3dev2.sound import Sound
+
+# Write your program here
+brick.sound.beep()
+
+sound = Sound()
+sound.speak('Welcome to the E V 3 dev project!')
